89643b0740e75a6b5738b24e90eaf38ff4ddccc9
Francis Kurupacheril <navin@Francis-Kurupacherils-Mini.local>
2011-11-02 21:04:37 -0700

    Initial Commit

---
+++ Hello World/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Hello World
+//
+//  Created by Francis Kurupacheril on 11/2/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Hello WorldTests/Hello_WorldTests.h
@@ -0,0 +1,13 @@
+//
+//  Hello_WorldTests.h
+//  Hello WorldTests
+//
+//  Created by Francis Kurupacheril on 11/2/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Hello_WorldTests : SenTestCase
+
+@end
